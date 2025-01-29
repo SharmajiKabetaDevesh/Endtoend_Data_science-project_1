d6771ec29ebdadaaa4897fe921e532206faf5897
SharmajiKabetaDevesh <deveshkumar.sharma@dmce.ac.in>
2025-01-29 18:45:43 +0530

    completed the project

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,jsonify,request
+import os
+from src.datascience.pipeline.prediction_pipeline import PredictionPipeline
+import numpy as np
+import pandas as pd
+app= Flask("__main__")
+@app.route("/",methods=['GET'])
+def home():
+    return render_template("index.html")
+
+@app.route('/train',methods=['GET'])
+def training():
+    os.system("python main.py")
+    return "Training Successfull"
+
+@app.route("/predict",methods=['POST','GET'])
+def index():
+    if(request.method=='POST'):
+        try:
+            fixed_acidity =float(request.form['fixed_acidity'])
+            volatile_acidity =float(request.form['volatile_acidity'])
+            citric_acid =float(request.form['citric_acid'])
+            residual_sugar =float(request.form['residual_sugar'])
+            chlorides =float(request.form['chlorides'])
+            free_sulfur_dioxide =float(request.form['free_sulfur_dioxide'])
+            total_sulfur_dioxide =float(request.form['total_sulfur_dioxide'])
+            density =float(request.form['density'])
+            pH =float(request.form['pH'])
+            sulphates =float(request.form['sulphates'])
+            alcohol =float(request.form['alcohol'])
+            data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
+            data=np.array(data).reshape(1,11)
+            obj = PredictionPipeline()
+            output= obj.predict(data)
+            return render_template("results.html",prediction=output)
+        except Exception as e:
+            return("error")
+    else:
+        return render_template('index.html')
+    
+if __name__ =="__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
